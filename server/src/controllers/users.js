86a26d148eb3dc4213c3fa1826bfaa56f57e2f13
Apekshya <apu58728@gmail.com>
2024-01-25 00:29:36 +0545

    redux_setup

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,9 +1,12 @@
 const User = require('../models/users')
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const saltRounds = 10;
+
 const registerNewUser = async(req, res) => {
   try{
     const existingUser = await  User.findOne({email: req.body.email})
+    //if user email already exist, return 403, else create User doc
     if(existingUser){
      return res.status(403).json({
           msg: "User already exist"
@@ -21,4 +24,32 @@ const registerNewUser = async(req, res) => {
   }
 }
 
-module.exports = {registerNewUser}
+
+
+const loginUser = async (req, res) => {
+  try {
+    const userDetails = await User.findOne({ email: req.body.email });
+    if (userDetails) {
+      const matched = await bcrypt.compare(
+        req.body.password,
+        userDetails.password
+      );
+      if (matched) {
+        const token = jwt.sign(
+          { email: userDetails.email },
+          process?.env.SECRET_KEY
+        );
+        return res.status(201).json({ msg: "Login Successfully", token, userDetails });
+      } else {
+        return res.status(403).json({ msg: "Password didn't match" });
+      }
+    } else {
+      return res.status(401).json({ msg: "Email not found" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "Login failed" });
+  }
+};
+
+module.exports = {loginUser, registerNewUser}
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express')
-const {registerNewUser} = require('../controllers/users')
+const {registerNewUser,loginUser} = require('../controllers/users')
 router = express.Router();
 router.post('/register',registerNewUser )
+router.post('/login', loginUser)
+
 module.exports = router
